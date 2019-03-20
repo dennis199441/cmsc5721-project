76329dc9631716a79db2768bdb18832a5ace3882
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-03-21 00:28:15 +0800

    create toy lstm network

--- embedding_prediction.py
+++ embedding_prediction.py
@@ -18,51 +18,33 @@ python3 embedding_prediction.py --timescale 250 --threshold 0.6 --input_folder '
 python embedding_prediction.py --timescale 250 --threshold 0.6 --input_folder C:/Users/cwxxcheun/Desktop/Other/github/cmsc5721-project/network_data/daily_net/metadata_stocknet_timescale_250threshold_0.6 --embedding gcn --n_prev 30  --batch_size 200
 '''
 
-def load_pickle(args):
-	TIMESCALE = args.timescale
-	THRESHOLD = args.threshold
-	FOLDER_NAME =  args.input_folder + '/embeddings/'
-	EMBEDDING = args.embedding
-	EMBEDDING_MATRICES = None
-	target_file = FOLDER_NAME + 'embedding_matrices_' + EMBEDDING + '.pickle'
-	if os.path.exists(target_file):
-		pickle_in = open(target_file, "rb")
-		EMBEDDING_MATRICES = pickle.load(pickle_in)
-	else:
-		print(target_file + 'not exists')
+def generate_arrays_from_matrices(data, n_prev=2):
+	while True:
+		for i in range(len(data) - n_prev):		
+			X = np.array([np.array(data[i:i + n_prev])])
+			y = np.array([data[i + n_prev]])
+			yield (X, y)
 
-	return EMBEDDING_MATRICES
+def generate_test_data_from_matrices(data, n_prev=2):
+	while True:
+		for i in range(len(data) - n_prev):	
+			X = np.array([np.array(data[i:i + n_prev])])
+			yield (X)
 
 def reshape_matrices(matrices):
 	'''
-	Reshape matrix into column vector
+	Reshape matrix into row vector shape = (n,)
 	'''
 	for i in range(len(matrices)):
 		matrices[i] = matrices[i].reshape(-1,)
 
-def _load_data(data, n_prev=2):
-	'''
-	docX is the list of 3D numpy array
-	docY is the list of 2D numpy array
-	'''
-	docX, docY = [], []
-	for i in range(len(data) - n_prev):
-		msg = "Loading data %i of %i" % (i, len(data) - 1 - n_prev)
-		sys.stdout.write(msg + chr(8) * len(msg))
-		sys.stdout.flush()		
-		docX.append(np.array(data[i:i + n_prev]))
-		docY.append(np.array(data[i + n_prev]))
-
-	alsX = np.array(docX)
-	alsY = np.array(docY)
-
-	return alsX, alsY 
-
-def train_test_split(data, test_size=0.25, n_prev=2):
+def get_train_data(data, test_size=0.25):
 	ntrn = round(len(data) * (1 - test_size))
-	X_train, y_train = _load_data(data[0:ntrn], n_prev=n_prev)
-	X_test, y_test = _load_data(data[ntrn:], n_prev=n_prev)
-	return (X_train, y_train), (X_test, y_test)
+	return data[0:ntrn]
+
+def get_test_data(data, test_size=0.25):
+	ntrn = round(len(data) * (1 - test_size))
+	return data[ntrn:]
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -74,44 +56,32 @@ if __name__ == '__main__':
 	parser.add_argument('--batch_size', help="batch size", type=int, default=100)
 
 	args = parser.parse_args()
-	data = load_pickle(args)
-	reshape_matrices(data)
+	
+	# data = load_pickle(args)
+	# reshape_matrices(data)
+	data = np.array([np.array([random()*i, random()*i]) for i in range(1000)])
+
+	'''
+	Read Embedding file by file, instead of loading all embeddings into memory!!!
+	Read file should be done in generate_arrays_from_matrices()
+	'''
+
 	n_prev = args.n_prev
 	batch_size = args.batch_size
 
-	(X_train, y_train), (X_test, y_test) = train_test_split(data, test_size=0.25, n_prev=n_prev)
-
-	in_out_neurons = 117760
-	hidden_neurons = 5000
-	hidden_neurons_2 = 500
+	in_out_neurons = 2 ## 460 * 256
+	hidden_neurons = 50
 	model = Sequential()
 	model.add(LSTM(hidden_neurons, return_sequences=True, input_shape=(n_prev, in_out_neurons)))
-	model.add(LSTM(hidden_neurons_2, return_sequences=True, input_shape=(n_prev, hidden_neurons)))
-	model.add(LSTM(hidden_neurons, return_sequences=False, input_shape=(n_prev, hidden_neurons_2)))
+	model.add(LSTM(hidden_neurons, return_sequences=False, input_shape=(n_prev, hidden_neurons)))
 	model.add(Dense(in_out_neurons, input_dim=hidden_neurons))  
-	model.add(Activation("linear"))  
-	model.compile(loss="mean_squared_error", optimizer="rmsprop", metrics=["accuracy", 'mae', 'mape', 'mse'])
+	model.add(Activation("sigmoid"))  
+	model.compile(loss="mean_squared_error", optimizer="adam", metrics=["accuracy", 'mae', 'mape', 'mse'])
 
-
-	# and now train the model
-	# batch_size should be appropriate to your memory size
-	# number of epochs should be higher for real world problems
-	model.fit(X_train, y_train, batch_size=batch_size, epochs=10, validation_split=0.25) 
-	predicted = model.predict(X_test) 
-	score = model.evaluate(X_test, y_test, batch_size=batch_size*0.5)
+	model.fit_generator(generate_arrays_from_matrices(get_train_data(data), n_prev),steps_per_epoch=100, epochs=5)
+	predicted = model.predict_generator(generate_test_data_from_matrices(get_test_data(data), n_prev), steps=n_prev) 
+	score = model.evaluate_generator(generate_arrays_from_matrices(get_test_data(data), n_prev), steps=n_prev)
 
 	print("loss, accuracy, mae, mape, mse == {}".format(score))
-	print("len(y_test): {}".format(len(y_test)))
-
-	mse = (((predicted - y_test) ** 2).mean(axis=0))
-	print("len(mse)".format(len(mse)))
-
-	score2 = np.sqrt(np.mean(np.square(predicted - y_test)))
-	print ("score 2 == {}".format(score2))
-
-	# and maybe plot it
-	# plt.plot(pd.DataFrame(predicted[:20])) 
-	# plt.plot(pd.DataFrame(y_test[:20]))
-	# plt.show()
 	
 
