1cda17456755e4fa4c735471372e2c59a1fd81b9
Dennis Cheung <wcgnuehc@yahoo.com.hk>
2019-03-26 16:37:40 +0800

    update

---
+++ node_embedding_classification.py
@@ -0,0 +1,10 @@
+'''
+Task 2: Given A graph embedding matrix, classify if the stock price increased. 
+
+Given G_t: log(P_t) - log(P_t-1) > 0
+
+G: Graph embedding matrix
+
+feature = G_t
+target = r_t
+'''
