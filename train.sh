a434f3408a42477f3916a77cc7c9373e6ce17176
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-04-15 00:16:45 +0800

    update

--- train.sh
+++ train.sh
@@ -1,17 +1,17 @@
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 100 --epochs 1 --input_folder ./network_data
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 100 --epochs 1 --input_folder ./network_data
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 150 --epochs 1 --input_folder ./network_data
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 150 --epochs 500 --input_folder ./network_data/data
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding gcn --hidden_neurons 100 --epochs 1 --input_folder ./network_data --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding gcn --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding gcn --hidden_neurons 150 --epochs 1 --input_folder ./network_data --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding gcn --hidden_neurons 150 --epochs 500 --input_folder ./network_data/data --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 100 --epochs 1 --input_folder ./network_data --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 150 --epochs 1 --input_folder ./network_data --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 150 --epochs 500 --input_folder ./network_data/data --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 100 --epochs 1 --input_folder ./network_data --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 150 --epochs 1 --input_folder ./network_data --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 150 --epochs 500 --input_folder ./network_data/data --regularization True
--- train_adam.sh
+++ train_adam.sh
@@ -1,18 +1,18 @@
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 100 --epochs 1 --input_folder ./network_data --optimizer adam
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data --optimizer adam
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 100 --epochs 1 --input_folder ./network_data --optimizer adam
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data --optimizer adam
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 150 --epochs 1 --input_folder ./network_data --optimizer adam
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 150 --epochs 500 --input_folder ./network_data/data --optimizer adam
 
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding gcn --hidden_neurons 100 --epochs 1 --input_folder ./network_data --optimizer adam --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding gcn --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data --optimizer adam --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding gcn --hidden_neurons 150 --epochs 1 --input_folder ./network_data --optimizer adam --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding gcn --hidden_neurons 150 --epochs 500 --input_folder ./network_data/data --optimizer adam --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 100 --epochs 1 --input_folder ./network_data --optimizer adam --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data --optimizer adam --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 150 --epochs 1 --input_folder ./network_data --optimizer adam --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding n2v --hidden_neurons 150 --epochs 500 --input_folder ./network_data/data --optimizer adam --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 100 --epochs 1 --input_folder ./network_data --optimizer adam --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 100 --epochs 500 --input_folder ./network_data/data --optimizer adam --regularization True
 
-python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 150 --epochs 1 --input_folder ./network_data --optimizer adam --regularization True
+python3 embedding_classification.py --timescale 250 --threshold 0.6 --embedding graphsage_mean --hidden_neurons 150 --epochs 500 --input_folder ./network_data/data --optimizer adam --regularization True
