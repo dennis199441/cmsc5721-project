a563b7fcad9d8f30241c1375ef9ea08939470527
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-04-23 00:25:00 +0800

    added risk free rate data

---
+++ risk_free.py
@@ -0,0 +1,22 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+def read_txt(filename):
+	res = []
+	with open(filename, "r") as f:
+		for line in f:
+			res.append(float(line))
+	return res
+
+if __name__ == '__main__':
+	res = read_txt('./risk_free.txt')
+	annual = np.mean(res) / 100
+	daily = (annual + 1)**(1/250) - 1
+	print("Annual risk free: {}".format(annual))
+	print("Daily risk free: {}".format(daily))
+	# plt.plot(res, label='risk free rate')
+	# plt.title('1-year T-bill rate (%)')
+	# plt.legend(loc='lower right')
+	# plt.show()
